d6f54743ebbda09a33fba0da212708575368f36d
SuperSonic <supersonic@livemail.tw>
2022-03-30 22:28:04 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  pages: {
+    index: {
+      title: 'Taiwan Web Technology Promotion Organization 臺灣網際網路技術推廣組織',
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/openchat/' : '/',
   transpileDependencies: true
 })
