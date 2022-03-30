7ef2a95a177ab564ebb331fd566302d3cd027b60
SuperSonic <supersonic@livemail.tw>
2022-03-30 23:28:13 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,14 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/rule',
+    name: 'rule',
+    component: () => import('../views/RuleView.vue')
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
---
+++ src/views/RuleView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="mx-10 my-10">
+    <div class="prose lg:prose-xl">
+      <h1>社群規範</h1>
+      <p>歡迎各位加入由 臺灣網際網路技術推廣組織 所管理的 LINE社群 owo</p>
+      <p>本組織為各位開發者、學生、研究人員......等等，在各領域能彼此切磋、詢問、提供協助的社群空間。</p>
+      <p>
+        但為維護社群安全及和平共處，
+        本社群除要求遵守<a href="http://official-blog.line.me/tw/archives/82859412.html">LINE社群使用條款</a>外，
+        並同時宣告以下自有社群規範：
+      </p>
+      <hr>
+      <ol>
+        <li>
+          <p>程式碼傳送/發佈</p>
+          <p>
+            為維護版面整潔，請求發問程式碼時，
+            應採用「程式碼片段筆記」
+            或「原始碼版本控制」平台提供其原始碼。
+          </p>
+          <p>程式碼片段筆記平台：</p>
+          <ul>
+            <li><a href="https://gist.github.com">https://gist.github.com</a></li>
+            <li><a href="https://pastebin.com">https://pastebin.com</a></li>
+            <li><a href="https://gitlab.com/explore/snippets">https://gitlab.com/explore/snippets</a></li>
+          </ul>
+          <p>原始碼版本控制平台：</p>
+          <ul>
+            <li><a href="https://github.com">https://github.com</a></li>
+            <li><a href="https://gitlab.com">https://gitlab.com</a></li>
+            <li><a href="https://bitbucket.com">https://bitbucket.com</a></li>
+          </ul>
+          <p>亦歡迎使用類似服務以提供其原始碼。</p>
+        </li>
+        <li>
+          <p>徵才廣告</p>
+          <p>
+            本社群歡迎各大中小企業來往徵才。
+            為維護版面整潔，「嚴禁」於聊天室發佈徵才廣告。
+            請前往本社群記事本中的徵才討論文下方留言，
+            以利管理員審核/管理...等。
+            同時，該徵才內容不得違反中華民國國家法規。
+          </p>
+        </li>
+        <li>
+          <p>第三方社群宣傳</p>
+          <p>
+            本社群開放第三方與資訊領域相關社群前來宣傳。
+            與第二點規範方式相同，
+            自本文張貼起，禁止於聊天室內發佈宣傳文章。
+            請前往對應之記事本討論串發佈。
+          </p>
+        </li>
+        <li>
+          <p>安全性政策</p>
+          <p>
+            為了維持社群安全，
+            嚴禁成員違反中華民國國家法規，
+            及作出任何危害社群權益之行為。
+          </p>
+          <p>管理員保有該項行使之最終解釋權。</p>
+        </li>
+        <li>
+          <p>懲處方案</p>
+          <p>
+            除第四點守則，
+            其他皆處以警告，
+            達到三次（含）即驅逐。
+          </p>
+          <p>第四點則以嚴重性定奪。</p>
+        </li>
+      </ol>
+      <hr>
+      <p>
+        為維持社群促進及安全，你我皆有責任。
+        感謝大家配合 &gt;&lt;
+      </p>
+    </div>
+    <div class="mt-10">
+      <router-link to="/">返回首頁</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RuleView.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
