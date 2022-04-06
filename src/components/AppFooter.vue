4856d6336fdeac873e2c2c7c515542a6fa910109
SuperSonic <supersonic@livemail.tw>
2022-04-06 12:56:24 +0800

    Update

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,36 +1,7 @@
 <template>
   <footer class="mt-16 pb-16 text-sm leading-6">
-    <div class="max-w-7xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-      <div class="flex">
-        <div class="flex-none w-1/2 space-y-10 sm:space-y-8 lg:flex lg:space-y-0">
-          <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">組織資訊</h2>
-            <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
-            </ul>
-          </div>
-          <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">學習資源</h2>
-            <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="flex-none w-1/2 space-y-10 sm:space-y-8 lg:flex lg:space-y-0">
-          <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">就業資源</h2>
-            <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
-            </ul>
-          </div>
-          <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">社交媒體</h2>
-            <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div class="max-w-7xl mx-auto mt-11 divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
+      「OpenChat Hub」是利用「<a href="https://line.me">LINE</a>社群」作為交流平台，由「臺灣網際網路技術推廣組織」所管理的一系列群組。
     </div>
     <div class="max-w-7xl mx-auto mt-11 divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
       &copy; 2022 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
