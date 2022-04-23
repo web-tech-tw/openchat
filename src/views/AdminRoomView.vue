011579bef44f0c36217a52f4d436c61eddda7969
SuperSonic <supersonic@livemail.tw>
2022-04-24 01:44:59 +0800

    Fix bugs

--- src/views/AdminRoomView.vue
+++ src/views/AdminRoomView.vue
@@ -23,9 +23,9 @@
           </button>
         </div>
         <div class="flex justify-end mt-4">
-          <button
+          <a
               class="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              :disabled="!ready"
+              :href="url"
           >
             <svg v-show="!ready" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
                  fill="none" viewBox="0 0 24 24">
@@ -34,7 +34,7 @@
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
             立即加入
-          </button>
+          </a>
         </div>
       </div>
     </div>
