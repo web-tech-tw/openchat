9712eb0b21669a6116d44e32517bb61c56f11c84
SuperSonic <supersonic@livemail.tw>
2022-11-04 17:01:19 +0000

    Update rules

--- src/views/RuleView.vue
+++ src/views/RuleView.vue
@@ -2,7 +2,12 @@
   <div class="my-9 flex justify-center mx-5">
     <div class="prose lg:prose-xl">
       <h1>OpenChat Hub 規範</h1>
-      <p>歡迎各位加入由 臺灣網際網路技術推廣組織 所管理的 LINE社群 owo</p>
+      <p>
+        本規範已於台北時間 2022年11月5日 上午1點2分 更新，煩請您詳細閱讀。
+      </p>
+      <p>
+        歡迎各位加入由 臺灣網際網路技術推廣組織 所管理的 LINE社群 owo
+      </p>
       <p>
         本組織為各位開發者、學生、研究人員......等等，提供了能在各領域彼此互相切磋、詢問、協助的社群空間。
       </p>
@@ -24,41 +29,81 @@
           <p>程式碼片段筆記平台：</p>
           <ul>
             <li>
-              <a href="https://gist.github.com"> https://gist.github.com </a>
+              <a href="https://web-tech-tw.github.io/chew.inte">Chew 口香糖</a>
             </li>
             <li>
-              <a href="https://pastebin.com"> https://pastebin.com </a>
+              <a href="https://pastebin.com">Pastebin</a>
             </li>
             <li>
-              <a href="https://gitlab.com/explore/snippets">
-                https://gitlab.com/explore/snippets
-              </a>
+              <a href="https://gist.github.com">GitHub Gist</a>
+            </li>
+            <li>
+              <a href="https://gitlab.com/explore/snippets">GitLab Snippets</a>
             </li>
           </ul>
           <p>原始碼版本控制平台：</p>
           <ul>
-            <li><a href="https://github.com">https://github.com</a></li>
-            <li><a href="https://gitlab.com">https://gitlab.com</a></li>
-            <li><a href="https://bitbucket.com">https://bitbucket.com</a></li>
+            <li>
+              <a href="https://github.com">GitHub</a>
+            </li>
+            <li>
+              <a href="https://gitlab.com">GitLab</a>
+            </li>
+            <li>
+              <a href="https://bitbucket.com">Bitbucket</a>
+            </li>
           </ul>
           <p>亦歡迎使用類似服務以提供其原始碼。</p>
         </li>
         <li>
-          <p>徵才廣告</p>
+          <p>徵才資訊</p>
           <p>
-            本組織歡迎各大中小企業來往徵才。
-            為維護版面整潔，「嚴禁」於聊天室發佈徵才廣告。
-            請前往該社群記事本中的徵才討論文下方留言，
-            以利管理員審核/管理...等。
-            同時，該徵才內容不得違反中華民國國家法規。
+            本組織歡迎各大中小企業於 OpenChat Hub 進行徵才，
+            但僅允許為與該社群主題相關之徵才資訊，
+            該徵才內容亦不得違反中華民國國家法規。
+            若違反上述條件，或該資訊具有詐騙性質、危害公眾性質、具有安全性隱患者，
+            將會刪除與該徵才資訊相關聊天紀錄，同時將徵才者逐出，
+            OpenChat Hub 管理團隊對於「安全性隱患」定義具有最終解釋權。
           </p>
         </li>
         <li>
           <p>第三方社群宣傳</p>
           <p>
-            本組織開放第三方與資訊領域相關社群前來宣傳。 與第二點規範方式相同，
-            自本文張貼起，禁止於聊天室內發佈宣傳文章。
-            請前往對應之記事本討論串發佈。
+            本組織開放第三方與資訊領域相關社群前來宣傳，
+            該社群須與本聊天室主題相關，亦不得違反中華民國國家法規。
+            若違反上述條件，或該資訊具有詐騙性質、危害公眾性質、具有安全性隱患者，
+            將會刪除與該社群宣傳相關聊天紀錄，同時將宣傳者逐出，
+            OpenChat Hub 管理團隊對於「安全性隱患」定義具有最終解釋權。
+          </p>
+        </li>
+        <li>
+          <p>社群成員糾紛</p>
+          <p>
+            若社群成員於討論的過程產生糾紛，請以理性進行對談，
+            不得以私下聯絡進行談判。
+            倘若糾紛於糾紛開始 24 小時內無法得到解決，
+            且管理員或共同管理員調停無果，將會視為具有「安全性隱患」，
+            恕將與糾紛相關之人員逐出該社群。
+            OpenChat Hub 管理團隊對於「安全性隱患」定義具有最終解釋權。
+          </p>
+        </li>
+        <li>
+          <p>共同管理員</p>
+          <p>
+            共同管理員為志願服務性質，
+            若共同管理員違反本規範，將剝奪共同管理員權限，
+            並即刻逐出該社群。
+          </p>
+        </li>
+        <li>
+          <p>社群成員保護政策</p>
+          <p>
+            若在本組織的各種交流平台上，
+            遇到任何不公平對待、受到騷擾等情形。
+            若您遇到的狀況被允許公開，
+            歡迎利用我們 Discord 的 #組織方針 頻道進行「公開」申訴。
+            如有更多問題，
+            歡迎隨時標註我們 Discord 的 @security 身份組進行詢問。
           </p>
         </li>
         <li>
@@ -67,12 +112,7 @@
             為了維持社群安全，嚴禁成員違反中華民國國家法規，
             及作出任何危害社群權益之行為。
           </p>
-          <p>管理員保有該項行使之最終解釋權。</p>
-        </li>
-        <li>
-          <p>懲處方案</p>
-          <p>除第四點守則，其他皆處以警告，達到三次（含）即驅逐。</p>
-          <p>第四點則以嚴重性定奪。</p>
+          <p>OpenChat Hub 管理團隊保有該項行使之最終解釋權。</p>
         </li>
       </ol>
       <hr/>
