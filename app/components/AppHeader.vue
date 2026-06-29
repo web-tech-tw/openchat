d321b1f00e7bc36c2f9496510b18e33803203f74
不知火 Shiranui <supersonic@livemail.tw>
2026-06-29 14:58:34 +0800

    style: add relative positioning and z-index to header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-header">
+  <div class="app-header relative z-30">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
       <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
         <div class="flex justify-start lg:w-0 lg:flex-1">
